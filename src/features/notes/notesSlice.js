e70b3c0a5aeccae017588be2a36004f4d2bce268
Cihat KOÇAK <pcihatkocakks@gmail.com>
2022-03-06 23:50:23 +0300

    created-redux-and-simple-components

--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -3,17 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 export const notesSlice = createSlice({
     name: "notes",
     initialState: {
+        notes: [],
     },
-    reducers: {},
-    // Thunks
-    extraReducers: {
-    },
-  });
-  
+    reducers: {
+        addNote: (state, action) => {
+            state.notes.push(action.payload);
+        }
+    }
+});
 
-  
-
-  //for reducers
-  export const { addTodo, setFilter } = notesSlice.actions;
-  export default notesSlice.reducer;
-  
+export const { addNote } = notesSlice.actions;
+export default notesSlice.reducer;
